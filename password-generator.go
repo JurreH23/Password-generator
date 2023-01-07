60d938c3f8f2c77fb7631f2219e340899a967aca
JurreH23 <105643821+JurreH23@users.noreply.github.com>
2023-01-07 16:24:51 +0100

    Add files via upload

---
+++ password-generator.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/sethvargo/go-password/password"
+)
+
+var lengte, getallen, symbolen int
+
+func init() {
+	fmt.Println("Type hoe lang u uw wachtwoord wil maken.")
+	fmt.Scan(&lengte)
+	fmt.Println("Type hoeveel getallen u wilt genereren in uw wachtwoord")
+	fmt.Scan(&getallen)
+	fmt.Println("Type hoeveel symbolen u wilt genereren in uw wachtwoord")
+	fmt.Scan(&symbolen)
+}
+
+func main() {
+	wachtwoord()
+}
+
+func wachtwoord() {
+	res, err := password.Generate(lengte, getallen, symbolen, false, false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf(res)
+
+}
